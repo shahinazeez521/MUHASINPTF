3572e16f6970389c4907ba9d043bca620cf4bc4d
shahinazeez521 <shahinazeez521@gmail.com>
2025-09-21 18:19:31 +0530

    project section updated

--- assets/js/main.js
+++ assets/js/main.js
@@ -221,5 +221,21 @@
       }
     }
   });
+   document.addEventListener('DOMContentLoaded', function() {
+    const swiper = new Swiper('.portfolio-details-slider', {
+      slidesPerView: 1,
+      spaceBetween: 30,
+      loop: true,
+      grabCursor: true,
+      navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+      },
+      pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+      },
+    });
+  });
 
 })();
